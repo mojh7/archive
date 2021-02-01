6227809214a08d071af6ead2e101851c65471c3c
momo <mjh79017901@gmail.com>
2021-02-02 00:52:53 +0900

    PS: boj_5014_스타트링크 해결

--- problem_solving/boj/dfs_bfs/boj_5014_스타트링크.java
+++ problem_solving/boj/dfs_bfs/boj_5014_스타트링크.java
@@ -1,11 +1,45 @@
 /*
  * 2021-02-01
  * https://www.acmicpc.net/problem/5014
- * 백준 dfs bfs 골드 5
+ * 백준 bfs 골드 5
  *
 
-풀이 중
-37%에서 틀림
+37% 틀림 -> 해결
+
+------------
+최단 거리 찾는 문제이므로 bfs를 사용하는데
+입력이 10 1 10 3 2 일 경우 일일히 1에서 +3 +3 +3 찾지 않고 배수 만큼 스킵할 수 있다.
+그래서 1 + 3 * 3 = 10으로 답이 3이 나와야 되는데 
+
+10 7 10 1 15 같이 7 + 1 * 3 = 10, 목적지에 이미 도착했는데도 바로 cnt를 출력 안하고
+10 + up 혹은 10 + down 층을 검사하려고 하고 둘 다 조건에 맞지않아서 while문을 빠져나오고
+-1이 리턴되어 use the stairs 출력되게 해서 틀렸었다.
+
+if(s < g) {
+    if(u == 0) return -1;
+    cnt = (g - s) / u;
+    start += u * cnt;
+    if(cnt >= 1) {
+        visited[start - u] = cnt - 1;
+    }
+} else if(s > g){
+    if(d == 0) return -1;
+    cnt = (s - g) / d;
+    start -= d * cnt;
+    if(cnt >= 1) {
+        visited[start + d] = cnt - 1;
+    }
+}
+
+위에 조건을 아예 빼고 s부터 bfs로 탐색하던가 위의 조건문을 추가하면
+아래 조건 문을 추가 해줘야 틀리지 않고 정답.
+if(start == g) return cnt;
+
+s 부터 바로 bfs하는 것 보다 s -> g 갈 때 차이나는 만큼
+up 혹은 down 만큼의 배수를 먼저 카운팅 처리해주고 bfs 탐색하니 
+156ms -> 76ms 약간 더 빠르게 돌아간다.
+
+---
 
 사용 반례
 10 1 10 100 100
@@ -19,6 +53,7 @@
 
 1000 5 50 222 221
 답 90
+
  */
 
 import java.io.BufferedReader;
@@ -61,7 +96,7 @@ class Main {
         int nextUpFloor = 0;
         int nextDownFloor = 0;
         int cnt = 0;
-        
+
         if(s < g) {
             if(u == 0) return -1;
             cnt = (g - s) / u;
@@ -76,9 +111,8 @@ class Main {
             if(cnt >= 1) {
                 visited[start + d] = cnt - 1;
             }
-        } else {
-            return 0;
         }
+        if(start == g) return cnt;
 
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
@@ -90,12 +124,12 @@ class Main {
             nextUpFloor = floor + u;
             nextDownFloor = floor - d;
             if(u > 0 && nextUpFloor <= f && (visited[nextUpFloor] <= 0 ||
-                (visited[nextUpFloor] > 0 && cnt + 1 < visited[nextUpFloor]))) {
+                (cnt + 1 < visited[nextUpFloor]))) {
                 visited[nextUpFloor] = cnt + 1;
                 q.add(nextUpFloor);
             }
             if(d > 0 && nextDownFloor >= 1 && (visited[nextDownFloor] <= 0 ||
-                (visited[nextDownFloor] > 0 && cnt + 1 < visited[nextDownFloor]))) {
+                (cnt + 1 < visited[nextDownFloor]))) {
                 visited[nextDownFloor] = cnt + 1;
                 q.add(nextDownFloor);
             }
